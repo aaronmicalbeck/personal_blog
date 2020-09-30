945ff17916356b0e9b57e2cd30d96361d185be30
Aaron Beck <aaronmicalbeck@gmail.com>
2020-09-29 17:52:25 -0700

    package update

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -16,8 +16,8 @@ const navLinks = [
         path: '/contact'
     },
     {
-        title: 'Login',
-        path: '/login'
+        title: 'Archive',
+        path: '/archive'
     }
 ]
 
@@ -31,7 +31,7 @@ export default function Navigation({ user }) {
     return (
         <nav className="site-navigation">
 
-            <span className="menu-title">Responsive Nav Menu</span>
+            <span className="menu-title">The Self Inquiry</span>
             <div className={`menu-content-container ${menuActive && 'active'}`}>
             
                 <ul>
